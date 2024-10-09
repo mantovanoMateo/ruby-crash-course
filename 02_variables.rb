fec6114c0cbb4f2b350f393117699c0a6beda2f1
mateo <mantovanomateo@gmail.com>
2024-10-09 18:30:20 -0300

    First commit, modules 1, 2 & 3

---
+++ 02_variables.rb
@@ -0,0 +1,10 @@
+greetings_world = "Hello World"
+
+greetings = 50
+
+# Son constantes en ruby cuando inician con mayuscula
+Constant = 100
+
+puts greetings 
+puts greetings_world
+puts Constant
---
+++ 03_numbers.rb
@@ -0,0 +1,14 @@
+numA = 3
+numB = 10
+
+# result = numA + numB
+# result = numB - numA
+# result = numB * numA
+# result = numB % numA
+result = numB.to_i / numA.to_i
+
+puts result
+
+result = result.next
+
+puts result
